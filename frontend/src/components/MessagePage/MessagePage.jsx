2e7669bfe07971b8f6f02f00dfee7ec77784b084
endorpersand <daendrdragon@gmail.com>
2022-03-31 08:42:49 -0400

    oops, get rid of debug msg

--- frontend/src/components/MessagePage/MessagePage.jsx
+++ frontend/src/components/MessagePage/MessagePage.jsx
@@ -8,60 +8,6 @@ import MessageList from '../MessageList/MessageList';
 
 import {MESSAGE_IDLE, getMessage, MESSAGE_UPDATING, MESSAGE_SUCCESS} from '../../redux/message';
 
-function m(id) {
-    return {
-        users: {
-            "98765": {
-                'id': "98765",
-                'name': "ysrutnsrytunrs",
-                'nickname': "ysrutnsrytunrs",
-                'discriminator': "1291",
-                'avatar': "x"
-            },
-            "99999": {
-                'id': "98765",
-                'name': "usnysrutkrse",
-                'nickname': "usnysrutkrse",
-                'discriminator': "4140",
-                'avatar': "x"
-            }
-        },
-        channels: {
-            "54321": {
-                'id': "54321",
-                'type': 0,
-                'name': "unserious"
-            }
-        },
-        // roles: {str(role['snowflake']): {
-        //     'id': str(role['snowflake']),
-        //     'name': role['name'],
-        //     'color': role['color'],
-        //     'position': role['position']
-        // } for role in await db.fetch_all("SELECT * from referenced_roles WHERE nftid = :id", {'id': message_id})},
-        // attachments: {str(attachment['snowflake']): {
-        //     'id': str(attachment['snowflake']),
-        //     'url': attachment['url'],
-        //     'name': attachment['name'],
-        //     'height': attachment['height'],
-        //     'width': attachment['width'],
-        //     'spoiler': attachment['spoiler']
-        // } for attachment in await db.fetch_all("SELECT * from referenced_attachments WHERE nftid = :id", {'id': message_id})},
-        message: {
-            'messageID': "12345",
-            'channelID': "54321",
-            'guildID': "65432",
-            'authorID': "98765",
-            'content': "i am pinging <@99999>",
-            'mintedAt': Date.now(), // idk what .timestamp() is
-            'currentPrice': 9999,
-            'embeds': [],
-            'attachments': []
-        },
-        success: true
-    };
-}
-
 function Button(props) {
     return <button className={style["nftbutton"]} onClick={props.click}>{props.content}</button>;
 }
@@ -86,13 +32,11 @@ const MessagePage = () => {
         return <div>TODO: LOADING</div>
     }
 
-    // if (messageState.status !== MESSAGE_SUCCESS || !messageState.data.success) {
-    //     return <div>TODO: ERROR</div>
-    // }
+    if (messageState.status !== MESSAGE_SUCCESS || !messageState.data.success) {
+        return <div>TODO: ERROR</div>
+    }
 
-    // const messageData = messageState.data;
-
-    const messageData = m(id);
+    const messageData = messageState.data;
     let {message, users} = messageData;
 
     return (
