0ff5fe17db968ea5d02a75d2a09a073fc658a4e2
lexikiq <noellekiq@gmail.com>
2022-04-01 16:00:53 -0400

    feat: redo homepage text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import {WhiteButton, BlueButton} from "./components/Sea/SeaButton";
 function App() {
   return (
     <div className="App">
-      <h1>Don't discover, collect, and sell NFTs. Stop. What are you doing stop</h1>
-      <h2>HTSea is HTSTEM's first and only NFT marketplace thank god</h2>
+      <h1>Discover, collect, and sell HTNFTs.</h1>
+      <h2><i>The Forbes #1 Innovation in Technology and Computing for 2022</i></h2>
+      <h2>Buy one-of-a-kind centralized trinkets with our patent-pending Diamonds</h2>
+      <h2>Using revolutionary new technology, Diamonds can be obtained through a state-of-the-art
+      Minecraft server hosted at <b>mc.qixils.dev</b> or by solving games of Wordle</h2>
       <div className="button-wrapper">
-        <BlueButton>These buttons</BlueButton>
-        <WhiteButton>don't do anything</WhiteButton>
+        <BlueButton><a href={"/activity"}>Recent Activity</a></BlueButton>
+        <BlueButton><a href={"/wordle"}>Play Wordle</a></BlueButton>
+        <WhiteButton>mc.qixils.dev</WhiteButton>
       </div>
     </div>
   );
