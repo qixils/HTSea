b8d889b907161ca30a57e916b4e43d42c6f56ef5
endorpersand <daendrdragon@gmail.com>
2022-03-31 21:00:17 -0400

    some message page stuff

--- frontend/src/components/MessagePage/MessagePage.jsx
+++ frontend/src/components/MessagePage/MessagePage.jsx
@@ -1,15 +1,51 @@
 import style from './style.module.scss';
+import diamond from '../../icons/diamond.png';
 
 import {useCallback} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {useParams, Link} from 'react-router-dom';  
+import cls from 'classnames';
+import Popup from 'reactjs-popup';
 
 import MessageList from '../MessageList/MessageList';
 
 import {MESSAGE_IDLE, getMessage, MESSAGE_UPDATING, MESSAGE_SUCCESS} from '../../redux/message';
 
 function Button(props) {
-    return <button className={style["nftbutton"]} onClick={props.click}>{props.content}</button>;
+    return <Popup trigger={<button 
+        className={cls(style.nftbutton, {[style.nbb]: props.invert, [style.nbw]: !props.invert})}>
+            {props.content}
+        </button>} modal>
+            {props.children}
+        </Popup>;
+}
+
+function Price(props) {
+    return <div className={style["sale-price"]}>
+        <img className={style['diamond-icon']} src={diamond} width="24" height="26" alt="Diamonds" />
+        {props.price}
+    </div>
+}
+
+function BuyButton(props) {
+    let {msg} = props;
+    return <div className={style["sale-wrapper"]}>
+        {msg.currentPrice != null ?
+         <>
+            <div>Current price</div>
+            <Price price={msg.currentPrice} />
+         </> :
+         <>
+            <div>Highest offer</div>
+            <Price price="TODO" />
+         </>
+    }
+        <div className={style["nftbutton-wrapper"]}>
+            {(msg.currentPrice != null) ? 
+            <Button invert={true} onClick={()=>{}} content="Buy now" /> : null}
+            <Button onClick={()=>{}} content="Make offer" />
+        </div>
+    </div>;
 }
 
 const MessagePage = () => {
@@ -46,10 +82,7 @@ const MessagePage = () => {
                 <div className={style["author-header"]}>Owned by <Link to={`/users/${message.authorID}`}>{users[message.authorID].name}#{users[message.authorID].discriminator}</Link></div>
             </div>
             <MessageList messageData={messageData} />
-            {/* i thiiiiink it's Make offer OR Buy Price & Buy It Now, not both at the same time??? */}
-            <div>
-                <Button onClick={()=>{}} content="Make offer" />
-            </div>
+            <BuyButton msg={message} />
             {/* transaction table here */}
         </div>
     );
