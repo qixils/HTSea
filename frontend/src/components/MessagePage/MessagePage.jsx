84978a5cbfa9b1bacf2b063cf6641beed52cb279
endorpersand <daendrdragon@gmail.com>
2022-04-01 00:25:49 -0400

    SeaButton & start page

--- frontend/src/components/MessagePage/MessagePage.jsx
+++ frontend/src/components/MessagePage/MessagePage.jsx
@@ -4,19 +4,10 @@ import diamond from '../../icons/diamond.png';
 import {useCallback} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {useParams, Link} from 'react-router-dom';  
-import cls from 'classnames';
 import MessageList from '../MessageList/MessageList';
+import {BlueButton, WhiteButton} from '../Sea/SeaButton';
 
 import {MESSAGE_IDLE, getMessage, MESSAGE_UPDATING, MESSAGE_SUCCESS} from '../../redux/message';
-
-function Button(props) {
-    return (<button 
-            className={cls(style.nftbutton, {[style.nbb]: props.invert, [style.nbw]: !props.invert})}
-            onClick={props.onClick}
-            >
-                {props.content}
-        </button>);
-}
 
 function Price(props) {
     return <div className={style["sale-price"]}>
@@ -40,8 +31,8 @@ function BuyButton(props) {
     }
         <div className={style["nftbutton-wrapper"]}>
             {(msg.currentPrice != null) ? 
-            <Button invert={true} content="Buy now" /> : null}
-            <Button content="Make offer" />
+            <BlueButton>Buy now</BlueButton> : null}
+            <WhiteButton>Make offer</WhiteButton>
         </div>
     </div>;
 }
