e8b32598d94b40b269fc3546568abd7b3c961b02
endorpersand <daendrdragon@gmail.com>
2022-04-01 22:11:51 -0400

    fix chroma's image

--- frontend/src/components/MessageImage/MessageImage.jsx
+++ frontend/src/components/MessageImage/MessageImage.jsx
@@ -4,11 +4,15 @@ import style from './style.module.scss';
 // so use this JS to style them instead
 const sizeImageToFit = (width, height, maxWidth, maxHeight) => {
 	if (!width || !height) {
-		// fall back to CSS for dynamically-sized images
-		return `max-width: ${maxWidth}px; max-height: ${maxHeight}px; width: auto; height: auto;`;
+		return {
+			maxWidth: `${maxWidth}px`,
+			maxHeight: `${maxHeight}px`,
+			width: "auto",
+			height: "auto"
+		};
 	}
 
-	if (width < maxWidth && height < maxHeight) return '';
+	if (width < maxWidth && height < maxHeight) return;
 
 	const scaleFactor = Math.min(maxWidth / width, maxHeight / height);
 
