35cc934f25806e9b6f707949a9673850747d9036
valadaptive <valadaptive@protonmail.com>
2022-04-02 16:56:42 -0400

    Move paginator into its own component

--- frontend/src/components/UserPage/UserPage.jsx
+++ frontend/src/components/UserPage/UserPage.jsx
@@ -9,25 +9,25 @@ import Loader from '../Loader/Loader';
 import Diamonds from '../Diamonds/Diamonds';
 import MessageList from '../MessageList/MessageList';
 import ErrorPage from '../ErrorPage/ErrorPage';
+import Paginator from '../Paginator/Paginator';
 
 import {getUser, USER_IDLE, USER_UPDATING, USER_SUCCESS} from '../../redux/user';
 import api from '../../util/api';
-import classNames from 'classnames';
 
 const NFTPreview = ({id}) => {
     const [message, setMessage] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [loadState, setLoadState] = useState('idle');
 
-    if (message === null && !loading) {
-        setLoading(true);
+    if (message === null && loadState === 'idle') {
+        setLoadState('loading');
         api(`/api/messages/${id}`)
         .then(res => {
             setMessage(res);
-            setLoading(false);
+            setLoadState('loaded');
         })
         .catch(err => {
             setMessage(null);
-            setLoading(false);
+            setLoadState('error');
         });
     }
 
@@ -42,14 +42,11 @@ const NFTPreview = ({id}) => {
     );
 };
 
-const ITEMS_PER_PAGE = 5;
-
 const UserPage = () => {
     const {id} = useParams();
     const userState = useSelector(state => state.user);
     const dispatch = useDispatch();
     const getUserCallback = useCallback(() => getUser(dispatch, id), [dispatch, id]);
-    const [page, setPage] = useState(0);
 
     if (
         (userState.status === USER_IDLE) ||
@@ -64,28 +61,11 @@ const UserPage = () => {
     }
 
     if (userState.status !== USER_SUCCESS) {
-        return <ErrorPage error={userState.error } />
+        return <ErrorPage error={userState.error} />
     }
 
     const userData = userState.data;
     const {user} = userData;
-
-    const isFirstPage = page === 0;
-    const isLastPage = page >= Math.ceil(user.htnftIDs.length / ITEMS_PER_PAGE) - 1;
-
-    const paginator = (
-        <div className={style.paginator}>
-            <button
-                className={classNames(style.prev, style['page-button'])}
-                onClick={() => !isFirstPage && setPage(page - 1)}
-                disabled={isFirstPage}>←</button>
-            <div className={style['page-number']}>{user.htnftIDs.length > 0 ? page + 1 : 0}/{Math.ceil(user.htnftIDs.length / ITEMS_PER_PAGE)}</div>
-            <button
-                className={classNames(style.next, style['page-button'])}
-                onClick={() => !isLastPage && setPage(page + 1)}
-                disabled={isLastPage}>→</button>
-        </div>
-    );
 
     return (
         <div className={style['user-page']}>
@@ -99,12 +79,19 @@ const UserPage = () => {
                     <Diamonds diamonds={user.diamonds} />
                 </span>
             </div>
-            <h2>This user's HTNFTs: {paginator}</h2>
-            <div className={style.nfts}>
-                {user.htnftIDs.length > 0 ?
-                    user.htnftIDs.slice(page * ITEMS_PER_PAGE, (page + 1) * ITEMS_PER_PAGE).map(id => <NFTPreview id={id} key={id} />) :
-                    "This user doesn't own any HTNFTs."}
-            </div>
+            {user.htnftIDs.length > 0 ?
+                <Paginator
+                    items={user.htnftIDs}
+                    pageSize={5}
+                    header="This user's HTNFTs:"
+                    body={items => (
+                        <div className={style.htnfts}>
+                            {items.map(id => <NFTPreview id={id} key={id} />)}
+                        </div>)
+                    }
+                /> :
+                "This user doesn't own any HTNFTs."
+            }
         </div>
     );
 };
