473455209df92cfd909b8a857efe29a575b45b24
endorpersand <daendrdragon@gmail.com>
2022-04-02 17:01:52 -0400

    add api points for tx on user & msg pages

--- app/pkg/api/htnft.py
+++ app/pkg/api/htnft.py
@@ -346,7 +346,43 @@ async def transactions_to_api_response(txs):
 @route.get('/recent_transactions')
 async def recent_transactions(req: Request, resp: Response, before: float = None):
     if before is None: before = datetime.datetime.now().timestamp()
-    rows = await db.fetch_all("SELECT * FROM transactions WHERE timestamp < :before ORDER BY timestamp DESC LIMIT 10", {'before': datetime.datetime.fromtimestamp(before)})
+    rows = await db.fetch_all("""
+        SELECT * FROM transactions 
+         WHERE timestamp < :before 
+         ORDER BY timestamp 
+          DESC LIMIT 10""", {
+        'before': datetime.datetime.fromtimestamp(before)
+    })
+
+    payload = await transactions_to_api_response(rows)
+    return JSONResponse(content=jsonable_encoder(payload))
+
+@route.get('/recent_transactions/user/{user_id}')
+async def recent_transactions_user(req: Request, resp: Response, user_id: int, before: float = None):
+    if before is None: before = datetime.datetime.now().timestamp()
+    rows = await db.fetch_all("""
+        SELECT * FROM transactions 
+         WHERE timestamp < :before AND (buyer = :uid OR seller = :uid) 
+         ORDER BY timestamp 
+          DESC LIMIT 10""", {
+        'before': datetime.datetime.fromtimestamp(before),
+        'uid': user_id
+    })
+
+    payload = await transactions_to_api_response(rows)
+    return JSONResponse(content=jsonable_encoder(payload))
+
+@route.get('/recent_transactions/message/{message_id}')
+async def recent_transactions_message(req: Request, resp: Response, message_id: int, before: float = None):
+    if before is None: before = datetime.datetime.now().timestamp()
+    rows = await db.fetch_all("""
+        SELECT * FROM transactions 
+         WHERE timestamp < :before AND message = :mid 
+         ORDER BY timestamp 
+          DESC LIMIT 10""", {
+        'before': datetime.datetime.fromtimestamp(before),
+        'mid': message_id
+    })
 
     payload = await transactions_to_api_response(rows)
     return JSONResponse(content=jsonable_encoder(payload))
