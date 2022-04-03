a34f59f5d28e1468bf0f537d601449b562803982
endorpersand <daendrdragon@gmail.com>
2022-04-03 01:43:44 -0400

    avatar patch?

--- frontend/src/components/Avatar/Avatar.jsx
+++ frontend/src/components/Avatar/Avatar.jsx
@@ -1,19 +1,25 @@
 import style from './style.module.scss';
 
 const Avatar = ({user, size}) => {
-    if (!user) return `https://cdn.discordapp.com/embed/avatars/0.png`;
-    if (!user.avatar) return `https://cdn.discordapp.com/embed/avatars/${Number(user.discriminator) % 5}.png`;
-    // test if handed an avatar hash directly
-    let avatarHashMatch = /^([a-z0-9]+)$/.exec(user.avatar);
-    if (!avatarHashMatch) {
-        // extract avatar hash from CDN URL
-        avatarHashMatch = /^https?:\/\/cdn\.discordapp\.com\/avatars\/\d+\/([a-z0-9]+)\.[a-z]+$/.exec(user.avatar);
+    let avatarURL;
+    if (!user) {
+        avatarURL = `https://cdn.discordapp.com/embed/avatars/0.png`;
+    } else if (!user.avatar) {
+        avatarURL = `https://cdn.discordapp.com/embed/avatars/${Number(user.discriminator) % 5}.png`;
+    } else {
+        // test if handed an avatar hash directly
+        let avatarHashMatch = /^([a-z0-9]+)$/.exec(user.avatar);
+        if (!avatarHashMatch) {
+            // extract avatar hash from CDN URL
+            avatarHashMatch = /^https?:\/\/cdn\.discordapp\.com\/avatars\/\d+\/([a-z0-9]+)\.[a-z]+$/.exec(user.avatar);
+        }
+        if (avatarHashMatch) {
+            const avatarHash = avatarHashMatch[1];
+            avatarURL = `https://cdn.discordapp.com/avatars/${user.id || user.snowflake}/${avatarHash}.webp?size=${size}`;
+        } else {
+            avatarURL = user.avatar;
+        }
     }
-    if (!avatarHashMatch) {
-        return user.avatar;
-    }
-    const avatarHash = avatarHashMatch[1];
-    const avatarURL = `https://cdn.discordapp.com/avatars/${user.id || user.snowflake}/${avatarHash}.webp?size=${size}`;
 
     return (
         <img
